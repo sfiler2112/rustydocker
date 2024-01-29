08904b2851903da078d14713c83464bf2d7f4084
sfiler2112 <sfiler2011@gmail.com>
2024-01-29 16:53:08 -0500

    Canvas practice

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,63 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::response::content;
+
+
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, Docker!"
+    "Hello, Docker! Add 'worm' to the URL to play worm..."
+}
+
+#[get("/dirtyworm")]
+fn dirtyworm() -> content::RawHtml<&'static str> {
+    content::RawHtml(
+        "<canvas id=\"dddc\" width=\"150\" height=\"150\">
+            Dirty deeds done dirt cheap    
+        </canvas>
+        <script>
+            function draw() {
+                const canvas = document.getElementById(\"dddc\");
+                if(canvas.getContext) {
+                    const ctx = canvas.getContext(\"2d\");
+                    ctx.font = \"10px serif\";
+                    ctx.fillText(\"Dirty worm consumes\", 10, 50);
+                }
+            }
+            window.addEventListener(\"load\", draw);
+        </script>")
+}
+
+#[get("/worm")]
+fn worm() -> content::RawHtml<&'static str> {
+    content::RawHtml(
+        "<html lang=\"en-US\">
+            <head>
+            </head>
+            <body>
+                <canvas id=\"dddc\" width=\"150\" height=\"150\">
+                    Dirty deeds done dirt cheap    
+                </canvas>
+                <script>
+                    function draw() {
+                        const canvas = document.getElementById(\"dddc\");
+                        if(canvas.getContext) {
+                            const ctx = canvas.getContext(\"2d\");
+                            ctx.font = \"10px serif\";
+                            ctx.fillText(\"Silly worm consumes\", 10, 50);
+                        }
+                    }
+                    window.addEventListener(\"load\", draw);
+                </script>
+            </body>
+        </html>"
+    )
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+    .mount("/", routes![index])
+    .mount("/butt", routes![worm])
+    .mount("/", routes![dirtyworm])
 }
