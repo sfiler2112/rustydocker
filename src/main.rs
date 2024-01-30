4d70515b59b8f4fc44ad4505d073d3556bb3d10e
sfiler2112 <sfiler2011@gmail.com>
2024-01-30 17:17:26 -0500

    webgl tangent

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,23 @@ fn dirtyworm() -> content::RawHtml<&'static str> {
         "<canvas id=\"dddc\" width=\"150\" height=\"150\">
             Dirty deeds done dirt cheap    
         </canvas>
+        <script
+            src=\"https://cdnjs.cloudflare.com/ajax/libs/gl-matrix/2.8.1/gl-matrix-min.js\"
+            integrity=\"sha512-zhHQR0/H5SEBL3Wn6yYSaTTZej12z0hVZKOv3TwCUXT1z5qeqGcXJLLrbERYRScEDDpYIJhPC1fk31gqR783iQ==\"
+            crossorigin=\"anonymous\"
+        defer>
+        </script>
         <script>
             function draw() {
                 const canvas = document.getElementById(\"dddc\");
                 if(canvas.getContext) {
-                    const ctx = canvas.getContext(\"2d\");
-                    ctx.font = \"10px serif\";
-                    ctx.fillText(\"Dirty worm consumes\", 10, 50);
+                    //const ctx = canvas.getContext(\"2d\");
+                    const wgl = canvas.getContext(\"webgl\");
+
+                    //ctx.font = \"10px serif\";
+                    //ctx.fillText(\"Dirty worm consumes\", 10, 50);
+                    wgl.clearColor(0.0, 0.0, 0.0, 1.0);
+                    wgl.clear(wgl.COLOR_BUFFER_BIT);
                 }
             }
             window.addEventListener(\"load\", draw);
